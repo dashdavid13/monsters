6d53fe8b18aa83f5b8a4dfe9790e9b8099c707e8
David <davidamedina619@gmail.com>
2021-08-15 13:27:43 -0400

    cleaning

--- src/Components/search/search.component.jsx
+++ src/Components/search/search.component.jsx
@@ -9,18 +9,18 @@ export const Search = ({placeholder, handleChange}) => {
     arr.sort((a, b) => a - b);
   
     const arrayMapped = arr.map((e, i) => {
-      debugger
+      
         let object = {};
         object.values = [e, arr[i + 1]];
         object.dif = Math.abs(e - arr[i + 1]);
         return object;
-        debugger
+        
     });
     return arrayMapped.sort((a, b) => a.dif - b.dif )
         .filter((v) => v.dif == arrayMapped[0].dif)
         .map((v1) => v1.values)
         .flat();
-        debugger
+        
 }
 
 closestNumbers([4,5,6,2,1])
