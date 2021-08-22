675c6e7b979968b436af8bd1008fb347e73a84f6
David <davidamedina619@gmail.com>
2021-08-22 16:24:22 -0400

    removed function clicked

--- src/Components/search/search.component.jsx
+++ src/Components/search/search.component.jsx
@@ -5,23 +5,7 @@ import './search.styles.css'
 export const Search = ({placeholder, handleChange}) => {
 
 
-  function closestNumbers(arr) {
-    arr.sort((a, b) => a - b);
   
-    const arrayMapped = arr.map((e, i) => {
-      
-        let object = {};
-        object.values = [e, arr[i + 1]];
-        object.dif = Math.abs(e - arr[i + 1]);
-        return object;
-        
-    });
-    return arrayMapped.sort((a, b) => a.dif - b.dif )
-        .filter((v) => v.dif == arrayMapped[0].dif)
-        .map((v1) => v1.values)
-        .flat();
-        
-}
 
 closestNumbers([4,5,6,2,1])
 
